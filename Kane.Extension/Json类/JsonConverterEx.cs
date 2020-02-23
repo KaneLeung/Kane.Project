ebf4c32602b3ac0abc24a08b18af455c09ee52be
梁启兴 <2396635762@qq.com>
2020-02-24 00:38:02 +0800

    :rocket:更新Json扩展类

---
+++ Kane.Extension/Json类/JsonConverterEx.cs
@@ -0,0 +1,178 @@
+﻿#region << 版 本 注 释 >>
+/*-----------------------------------------------------------------
+* 项目名称 ：Kane.Extension
+* 项目描述 ：通用扩展工具
+* 类 名 称 ：JsonConverterEx
+* 类 描 述 ：自定义JsonConverter扩展
+* 所在的域 ：KK-HOME
+* 命名空间 ：Kane.Extension
+* 机器名称 ：KK-HOME 
+* CLR 版本 ：4.0.30319.42000
+* 作　　者 ：Kane Leung
+* 创建时间 ：2020/2/19 23:23:07
+* 更新时间 ：2020/2/19 23:23:07
+* 版 本 号 ：v1.0.0.0
+*******************************************************************
+* Copyright @ Kane Leung 2020. All rights reserved.
+*******************************************************************
+-----------------------------------------------------------------*/
+#endregion
+#if (NETCOREAPP3_0 || NETCOREAPP3_1)
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Kane.Extension
+{
+    /// <summary>
+    /// 自定义JsonConverter扩展
+    /// </summary>
+    public class JsonConverterEx
+    {
+        #region 自定义IntConverter + IntConverter : JsonConverter<int>
+        /// <summary>
+        /// 自定义IntConverter
+        /// </summary>
+        public class IntConverter : JsonConverter<int>
+        {
+            #region 转换格式的私有变量
+            /// <summary>
+            /// 失败返回的默认值私有变量
+            /// </summary>
+            private readonly int returnValue;
+            #endregion
+
+            #region 【构造函数】失败后返回默认值【0】 + IntConverter()
+            /// <summary>
+            /// 【构造函数】失败后返回默认值【0】
+            /// </summary>
+            public IntConverter() => returnValue = 0;
+            #endregion
+
+            #region 【构造函数】失败后返回用户自定义默认值 + IntConverter(int value)
+            /// <summary>
+            /// 【构造函数】失败后返回用户自定义默认值
+            /// </summary>
+            /// <param name="value">用户自定义默认值</param>
+            public IntConverter(int value) => returnValue = value;
+            #endregion
+
+
+            #region 重写转换器Read方法 + Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+            /// <summary>
+            /// 重写转换器Read方法
+            /// </summary>
+            /// <param name="reader"></param>
+            /// <param name="typeToConvert"></param>
+            /// <param name="options"></param>
+            /// <returns></returns>
+            public override int Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+            {
+                if (reader.TokenType == JsonTokenType.String && int.TryParse(reader.GetString(), out int result)) return result;
+                else if (reader.TokenType == JsonTokenType.Number && reader.TryGetInt32(out result)) return result;
+                else return returnValue;
+            }
+            #endregion
+
+            #region 重写转换器Write方法 + Write(Utf8JsonWriter writer, bool value, JsonSerializerOptions options) => writer.WriteBooleanValue(value);
+            /// <summary>
+            /// 重写转换器Write方法
+            /// </summary>
+            /// <param name="writer"></param>
+            /// <param name="value"></param>
+            /// <param name="options"></param>
+            public override void Write(Utf8JsonWriter writer, int value, JsonSerializerOptions options) => writer.WriteStringValue(value.ToString());
+            #endregion
+        }
+        #endregion
+
+        #region 自定义DateTimeConverter + DateTimeConverter : JsonConverter<DateTime>
+        /// <summary>
+        /// 自定义DateTimeConverter
+        /// https://github.com/dotnet/runtime/blob/master/src/libraries/System.Text.Json/src/System/Text/Json/Serialization/Converters/JsonValueConverterDateTime.cs
+        /// </summary>
+        public class DateTimeConverter : JsonConverter<DateTime>
+        {
+            #region 转换格式的私有变量
+            /// <summary>
+            /// 转换格式的私有变量
+            /// </summary>
+            private readonly string _format;
+            #endregion
+
+            #region 【构造函数】转换器默认格式【yyyy-MM-dd HH:mm:ss】 + DateTimeConverter()
+            /// <summary>
+            /// 【构造函数】转换器默认格式【yyyy-MM-dd HH:mm:ss】
+            /// </summary>
+            public DateTimeConverter() => _format = "yyyy-MM-dd HH:mm:ss";
+            #endregion
+
+            #region 【构造函数】转换器自定义格式 + DateTimeConverter(string format)
+            /// <summary>
+            /// 【构造函数】转换器自定义格式
+            /// </summary>
+            /// <param name="format">用户自定义格式</param>
+            public DateTimeConverter(string format) => _format = format;
+            #endregion
+
+            #region 重写转换器Read方法 + Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+            /// <summary>
+            /// 重写转换器Read方法
+            /// </summary>
+            /// <param name="reader"></param>
+            /// <param name="typeToConvert"></param>
+            /// <param name="options"></param>
+            /// <returns></returns>
+            public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) => reader.GetString().ToDT(_format);
+            #endregion
+
+            #region 重写转换器Write方法 + Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
+            /// <summary>
+            /// 重写转换器Write方法
+            /// </summary>
+            /// <param name="writer"></param>
+            /// <param name="value"></param>
+            /// <param name="options"></param>
+            public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options) => writer.WriteStringValue(value.ToString(_format));
+            #endregion
+        }
+        #endregion
+
+        #region 自定义BoolConverter + BoolConverter : JsonConverter<bool>
+        /// <summary>
+        /// System.Text.Json中接收的json中不能将 "true"/"false"识别为boolean的True/False，这也需要自定义Converter实现bool转换
+        /// https://github.com/dotnet/runtime/blob/master/src/libraries/System.Text.Json/src/System/Text/Json/Serialization/Converters/JsonValueConverterBoolean.cs
+        /// </summary>
+        public class BoolConverter : JsonConverter<bool>
+        {
+            #region 重写转换器Read方法 + Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+            /// <summary>
+            /// 重写转换器Read方法
+            /// </summary>
+            /// <param name="reader"></param>
+            /// <param name="typeToConvert"></param>
+            /// <param name="options"></param>
+            /// <returns></returns>
+            public override bool Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+            {
+                if (reader.TokenType == JsonTokenType.True || reader.TokenType == JsonTokenType.False) return reader.GetBoolean();
+                return reader.GetString().ToBool();
+            }
+            #endregion
+
+            #region 重写转换器Write方法 + Write(Utf8JsonWriter writer, bool value, JsonSerializerOptions options) => writer.WriteBooleanValue(value);
+            /// <summary>
+            /// 重写转换器Write方法
+            /// </summary>
+            /// <param name="writer"></param>
+            /// <param name="value"></param>
+            /// <param name="options"></param>
+            public override void Write(Utf8JsonWriter writer, bool value, JsonSerializerOptions options) => writer.WriteBooleanValue(value);
+            #endregion
+        }
+        #endregion
+    }
+}
+#endif
--- Kane.Extension/Json类/JsonEx.cs
+++ Kane.Extension/Json类/JsonEx.cs
@@ -17,14 +17,15 @@
 *******************************************************************
 -----------------------------------------------------------------*/
 #endregion
-using System;
-using System.Collections.Generic;
-using System.Text;
 #if (NETCOREAPP3_0 || NETCOREAPP3_1)
 using System.Text.Json;
+using System.Text.Unicode;
+using System.Text.Encodings.Web;
 #else
 using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 #endif
+
 
 namespace Kane.Extension
 {
@@ -33,38 +34,152 @@ namespace Kane.Extension
     /// </summary>
     public static class JsonEx
     {
-        #region 把对象序列化，转成Json字符串 + ToJson<T>(this T value)
-        /// <summary>
-        /// 把对象序列化，转成Json字符串
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="value">要序列化的对象</param>
-        /// <returns></returns>
-        public static string ToJson<T>(this T value)
-        {
 #if (NETCOREAPP3_0 || NETCOREAPP3_1)
-            return JsonSerializer.Serialize(value);
-#else
-            return JsonConvert.SerializeObject(value);
-#endif
+        /// <summary>
+        /// 全局序列化和反序列化的配置选项
+        /// </summary>
+        public static JsonSerializerOptions GlobalOption = DefaultOption();
+
+        #region 自用的【System.Text.Json】序列化和反序列化的配置选项 + DefaultOption()
+        /// <summary>
+        /// 自用的【System.Text.Json】序列化和反序列化的配置选项
+        /// </summary>
+        /// <returns></returns>
+        private static JsonSerializerOptions DefaultOption()
+        {
+            JsonSerializerOptions options = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = null,//保持属性名称不变
+                AllowTrailingCommas = true,//忽略多余的逗号
+                IgnoreNullValues = true,//忽略Null值
+                //PropertyNameCaseInsensitive = true;//反序列化是否不区分大小写
+            };
+            options.Converters.Add(new JsonConverterEx.DateTimeConverter());//使用【2020-02-21 17:06:15】时间格式
+            options.Converters.Add(new JsonConverterEx.BoolConverter());//"true"/"false"识别为boolean的True/False
+            options.Converters.Add(new JsonConverterEx.IntConverter());//"88"转为Int
+            options.Encoder = JavaScriptEncoder.Create(UnicodeRanges.All);//中文不会被编码
+            options.Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping;
+            return options;
         }
         #endregion
 
-        #region Json字符串反序列化，转成对象 + ToObject<T>(this string value)
+        #region 把对象序列化，转成Json字符串，使用默认配置选项 + ToJson<T>(this T value, JsonSerializerOptions options = null)
         /// <summary>
-        /// Json字符串反序列化，转成对象
+        /// 把对象序列化，转成Json字符串，使用默认配置选项
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="value">要序列化的对象</param>
+        /// <param name="options">序列化参数</param>
+        /// <returns></returns>
+        public static string ToJson<T>(this T value, JsonSerializerOptions options = null) => JsonSerializer.Serialize(value, options ?? GlobalOption);
+        #endregion
+
+        #region 把对象序列化，转成Json字符串 + ToJson<T>(this T value, bool ignore)
+        /// <summary>
+        /// 把对象序列化，转成Json字符串，可忽略默认配置选项
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="value">要序列化的对象</param>
+        /// <param name="ignore">是否忽略默认配置选项</param>
+        /// <returns></returns>
+        public static string ToJson<T>(this T value, bool ignore) => ignore ? JsonSerializer.Serialize(value) : JsonSerializer.Serialize(value, GlobalOption);
+        #endregion
+
+        #region Json字符串反序列化，转成对象,使用默认配置选项 + ToObject<T>(this string value, JsonSerializerOptions options = null)
+        /// <summary>
+        /// Json字符串反序列化，转成对象，使用默认配置选项
         /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <param name="value">Json字符串</param>
+        /// <param name="options">序列化参数</param>
         /// <returns></returns>
-        public static T ToObject<T>(this string value)
-        {
-#if (NETCOREAPP3_0 || NETCOREAPP3_1)
-            return JsonSerializer.Deserialize<T>(value);
+        public static T ToObject<T>(this string value, JsonSerializerOptions options = null) => JsonSerializer.Deserialize<T>(value, options ?? GlobalOption);
+        #endregion
+
+        #region Json字符串反序列化，转成对象 + ToObject<T>(this string value, bool ignore)
+        /// <summary>
+        /// Json字符串反序列化，转成对象，可忽略默认配置选项
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="value">Json字符串</param>
+        /// <param name="ignore">是否忽略默认配置选项</param>
+        /// <returns></returns>
+        public static T ToObject<T>(this string value, bool ignore)
+            => ignore ? JsonSerializer.Deserialize<T>(value) : JsonSerializer.Deserialize<T>(value, GlobalOption);
+        #endregion
 #else
-            return JsonConvert.DeserializeObject<T>(value);
-#endif
+        #region 自用的【Newtonsoft.Json】序列化和反序列化的配置选项 + DefaultSetting()
+        /// <summary>
+        /// 全局序列化和反序列化的配置选项
+        /// </summary>
+        public static JsonSerializerSettings GlobalSetting = DefaultSetting();
+
+        /// <summary>
+        /// 自用的【Newtonsoft.Json】序列化和反序列化的配置选项
+        /// </summary>
+        /// <returns></returns>
+        private static JsonSerializerSettings DefaultSetting()
+        {
+            return new JsonSerializerSettings
+            {
+                //日期类型默认格式化处理
+                DateFormatHandling = DateFormatHandling.MicrosoftDateFormat,
+                DateFormatString = "yyyy-MM-dd HH:mm:ss",
+                //空值处理
+                NullValueHandling = NullValueHandling.Ignore,
+                //设置不处理循环引用
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+                //使用默认方式，保持属性名称不变
+                ContractResolver = new DefaultContractResolver()
+            };
         }
         #endregion
+
+        #region 把对象序列化，转成Json字符串，使用默认配置选项 + ToJson<T>(this T value, JsonSerializerSettings settings = null)
+        /// <summary>
+        /// 把对象序列化，转成Json字符串，使用默认配置选项
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="value">要序列化的对象</param>
+        /// <param name="settings">序列化参数</param>
+        /// <returns></returns>
+        public static string ToJson<T>(this T value, JsonSerializerSettings settings = null) => JsonConvert.SerializeObject(value, settings ?? GlobalSetting);
+        #endregion
+
+        #region 把对象序列化，转成Json字符串，可忽略默认配置选项 + ToJson<T>(this T value, bool ignore)
+        /// <summary>
+        /// 把对象序列化，转成Json字符串，可忽略默认配置选项
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="value">要序列化的对象</param>
+        /// <param name="ignore">是否忽略默认配置选项</param>
+        /// <returns></returns>
+        public static string ToJson<T>(this T value, bool ignore) => ignore ? JsonConvert.SerializeObject(value) : JsonConvert.SerializeObject(value, GlobalSetting);
+        #endregion
+
+        #region Json字符串反序列化，转成对象 + ToObject<T>(this string value, JsonSerializerSettings settings = null)
+        /// <summary>
+        /// Json字符串反序列化，转成对象，使用默认配置选项
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="value">Json字符串</param>
+        /// <param name="settings">序列化参数</param>
+        /// <returns></returns>
+        public static T ToObject<T>(this string value, JsonSerializerSettings settings = null)
+            => JsonConvert.DeserializeObject<T>(value, settings ?? GlobalSetting);
+        #endregion
+
+        #region Json字符串反序列化，转成对象，可忽略默认配置选项 + ToObject<T>(this string value)
+        /// <summary>
+        /// Json字符串反序列化，转成对象，可忽略默认配置选项
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="value">Json字符串</param>
+        /// <param name="ignore">是否忽略默认配置选项</param>
+        /// <returns></returns>
+        public static T ToObject<T>(this string value, bool ignore)
+            => ignore ? JsonConvert.DeserializeObject<T>(value) : JsonConvert.DeserializeObject<T>(value, GlobalSetting);
+        #endregion
+#endif
     }
 }
