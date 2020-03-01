2154ae2fb6dd087179734540ac895bac1bb28c40
梁启兴 <2396635762@qq.com>
2020-03-01 23:17:11 +0800

    :heavy_plus_sign:+增加Netcore3.0和3.1的Json.Net扩展方法

--- Kane.Extension/Json类/JsonEx.cs
+++ Kane.Extension/Json类/JsonEx.cs
@@ -10,8 +10,8 @@
 * CLR 版本 ：4.0.30319.42000
 * 作　　者 ：Kane Leung
 * 创建时间 ：2020/1/15 23:29:15
-* 更新时间 ：2020/1/15 23:29:15
-* 版 本 号 ：v1.0.0.0
+* 更新时间 ：2020/03/01 21:29:15
+* 版 本 号 ：v1.0.1.0
 *******************************************************************
 * Copyright @ Kane Leung 2020. All rights reserved.
 *******************************************************************
@@ -21,10 +21,9 @@
 using System.Text.Json;
 using System.Text.Unicode;
 using System.Text.Encodings.Web;
-#else
+#endif
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
-#endif
 
 
 namespace Kane.Extension
@@ -34,85 +33,11 @@ namespace Kane.Extension
     /// </summary>
     public static class JsonEx
     {
-#if (NETCOREAPP3_0 || NETCOREAPP3_1)
-        /// <summary>
-        /// 全局序列化和反序列化的配置选项
-        /// </summary>
-        public static JsonSerializerOptions GlobalOption = DefaultOption();
-
-        #region 自用的【System.Text.Json】序列化和反序列化的配置选项 + DefaultOption()
-        /// <summary>
-        /// 自用的【System.Text.Json】序列化和反序列化的配置选项
-        /// </summary>
-        /// <returns></returns>
-        private static JsonSerializerOptions DefaultOption()
-        {
-            JsonSerializerOptions options = new JsonSerializerOptions
-            {
-                PropertyNamingPolicy = null,//保持属性名称不变
-                AllowTrailingCommas = true,//忽略多余的逗号
-                IgnoreNullValues = true,//忽略Null值
-                //PropertyNameCaseInsensitive = true;//反序列化是否不区分大小写
-            };
-            options.Converters.Add(new JsonConverterEx.DateTimeConverter());//使用【2020-02-21 17:06:15】时间格式
-            options.Converters.Add(new JsonConverterEx.BoolConverter());//"true"/"false"识别为boolean的True/False
-            options.Converters.Add(new JsonConverterEx.IntConverter());//"88"转为Int
-            options.Encoder = JavaScriptEncoder.Create(UnicodeRanges.All);//中文不会被编码
-            options.Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping;
-            return options;
-        }
-        #endregion
-
-        #region 把对象序列化，转成Json字符串，使用默认配置选项 + ToJson<T>(this T value, JsonSerializerOptions options = null)
-        /// <summary>
-        /// 把对象序列化，转成Json字符串，使用默认配置选项
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="value">要序列化的对象</param>
-        /// <param name="options">序列化参数</param>
-        /// <returns></returns>
-        public static string ToJson<T>(this T value, JsonSerializerOptions options = null) => JsonSerializer.Serialize(value, options ?? GlobalOption);
-        #endregion
-
-        #region 把对象序列化，转成Json字符串 + ToJson<T>(this T value, bool ignore)
-        /// <summary>
-        /// 把对象序列化，转成Json字符串，可忽略默认配置选项
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="value">要序列化的对象</param>
-        /// <param name="ignore">是否忽略默认配置选项</param>
-        /// <returns></returns>
-        public static string ToJson<T>(this T value, bool ignore) => ignore ? JsonSerializer.Serialize(value) : JsonSerializer.Serialize(value, GlobalOption);
-        #endregion
-
-        #region Json字符串反序列化，转成对象,使用默认配置选项 + ToObject<T>(this string value, JsonSerializerOptions options = null)
-        /// <summary>
-        /// Json字符串反序列化，转成对象，使用默认配置选项
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="value">Json字符串</param>
-        /// <param name="options">序列化参数</param>
-        /// <returns></returns>
-        public static T ToObject<T>(this string value, JsonSerializerOptions options = null) => JsonSerializer.Deserialize<T>(value, options ?? GlobalOption);
-        #endregion
-
-        #region Json字符串反序列化，转成对象 + ToObject<T>(this string value, bool ignore)
-        /// <summary>
-        /// Json字符串反序列化，转成对象，可忽略默认配置选项
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="value">Json字符串</param>
-        /// <param name="ignore">是否忽略默认配置选项</param>
-        /// <returns></returns>
-        public static T ToObject<T>(this string value, bool ignore)
-            => ignore ? JsonSerializer.Deserialize<T>(value) : JsonSerializer.Deserialize<T>(value, GlobalOption);
-        #endregion
-#else
         #region 自用的【Newtonsoft.Json】序列化和反序列化的配置选项 + DefaultSetting()
         /// <summary>
         /// 全局序列化和反序列化的配置选项
         /// </summary>
-        public static JsonSerializerSettings GlobalSetting = DefaultSetting();
+        public static JsonSerializerSettings GlobalSetting;
 
         /// <summary>
         /// 自用的【Newtonsoft.Json】序列化和反序列化的配置选项
@@ -133,6 +58,101 @@ namespace Kane.Extension
                 ContractResolver = new DefaultContractResolver()
             };
         }
+        #endregion
+
+#if (NETCOREAPP3_0 || NETCOREAPP3_1)
+        #region 静态构造函数，保证只运行一次
+        /// <summary>
+        /// 静态构造函数，保证只运行一次
+        /// </summary>
+        static JsonEx()
+        {
+            #region 自用的【System.Text.Json】序列化和反序列化的配置选项
+            JsonSerializerOptions options = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = null,//保持属性名称不变
+                AllowTrailingCommas = true,//忽略多余的逗号
+                IgnoreNullValues = true,//忽略Null值
+                //PropertyNameCaseInsensitive = true;//反序列化是否不区分大小写
+            };
+            options.Converters.Add(new JsonConverterEx.DateTimeConverter());//使用【2020-02-21 17:06:15】时间格式
+            options.Converters.Add(new JsonConverterEx.BoolConverter());//"true"/"false"识别为boolean的True/False
+            options.Converters.Add(new JsonConverterEx.IntConverter());//"88"转为Int
+            options.Encoder = JavaScriptEncoder.Create(UnicodeRanges.All);//中文不会被编码
+            options.Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping;
+            #endregion
+            GlobalOption = options;
+            GlobalSetting = DefaultSetting();
+        }
+        #endregion
+
+        /// <summary>
+        /// 全局序列化和反序列化的配置选项
+        /// </summary>
+        public static JsonSerializerOptions GlobalOption;
+
+        #region 把对象序列化，转成Json字符串，使用默认配置选项 + ToJson<T>(this T value, JsonSerializerOptions options = null, bool useJsonNet = false, JsonSerializerSettings settings = null)
+        /// <summary>
+        /// 把对象序列化，转成Json字符串，使用默认配置选项
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="value">要序列化的对象</param>
+        /// <param name="options">序列化参数</param>
+        /// <param name="useJsonNet">是否使用<see cref="Newtonsoft.Json"/></param>
+        /// <param name="settings"><see cref="Newtonsoft.Json"/>序列化参数</param>
+        /// <returns></returns>
+        public static string ToJson<T>(this T value, JsonSerializerOptions options = null, bool useJsonNet = false, JsonSerializerSettings settings = null)
+            => useJsonNet ? JsonConvert.SerializeObject(value, settings ?? GlobalSetting) : System.Text.Json.JsonSerializer.Serialize(value, options ?? GlobalOption);
+        #endregion
+
+        #region 把对象序列化，转成Json字符串 + ToJson<T>(this T value, bool ignore, bool useJsonNet = false)
+        /// <summary>
+        /// 把对象序列化，转成Json字符串，可忽略默认配置选项
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="value">要序列化的对象</param>
+        /// <param name="ignore">是否忽略默认配置选项</param>
+        /// <param name="useJsonNet">是否使用<see cref="Newtonsoft.Json"/></param>
+        /// <returns></returns>
+        public static string ToJson<T>(this T value, bool ignore, bool useJsonNet = false)
+            => ignore ? (useJsonNet ? JsonConvert.SerializeObject(value) : System.Text.Json.JsonSerializer.Serialize(value))
+            : (useJsonNet ? JsonConvert.SerializeObject(value, GlobalSetting) : System.Text.Json.JsonSerializer.Serialize(value, GlobalOption));
+        #endregion
+
+        #region Json字符串反序列化，转成对象,使用默认配置选项 + ToObject<T>(this string value, JsonSerializerOptions options = null, bool useJsonNet = false, JsonSerializerSettings settings = null)
+        /// <summary>
+        /// Json字符串反序列化，转成对象，使用默认配置选项
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="value">Json字符串</param>
+        /// <param name="options">序列化参数</param>
+        /// <param name="useJsonNet">是否使用<see cref="Newtonsoft.Json"/></param>
+        /// <param name="settings"><see cref="Newtonsoft.Json"/>序列化参数</param>
+        /// <returns></returns>
+        public static T ToObject<T>(this string value, JsonSerializerOptions options = null, bool useJsonNet = false, JsonSerializerSettings settings = null)
+            => useJsonNet ? JsonConvert.DeserializeObject<T>(value, settings ?? GlobalSetting) : System.Text.Json.JsonSerializer.Deserialize<T>(value, options ?? GlobalOption);
+        #endregion
+
+        #region Json字符串反序列化，转成对象 + ToObject<T>(this string value, bool ignore, bool useJsonNet = false)
+        /// <summary>
+        /// Json字符串反序列化，转成对象，可忽略默认配置选项
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="value">Json字符串</param>
+        /// <param name="ignore">是否忽略默认配置选项</param>
+        /// <param name="useJsonNet">是否使用<see cref="Newtonsoft.Json"/></param>
+        /// <returns></returns>
+        public static T ToObject<T>(this string value, bool ignore, bool useJsonNet = false)
+            => ignore ? (useJsonNet ? JsonConvert.DeserializeObject<T>(value) : System.Text.Json.JsonSerializer.Deserialize<T>(value))
+            : (useJsonNet ? JsonConvert.DeserializeObject<T>(value, GlobalSetting) : System.Text.Json.JsonSerializer.Deserialize<T>(value, GlobalOption));
+        #endregion
+
+#else
+        #region 静态构造函数，保证只运行一次 + JsonEx()
+        /// <summary>
+        /// 静态构造函数，保证只运行一次
+        /// </summary>
+        static JsonEx() => GlobalSetting = DefaultSetting();
         #endregion
 
         #region 把对象序列化，转成Json字符串，使用默认配置选项 + ToJson<T>(this T value, JsonSerializerSettings settings = null)
