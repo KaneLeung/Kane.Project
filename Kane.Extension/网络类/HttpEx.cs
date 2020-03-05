ce3725d9e7f266fbd8ee613c289e3064e3136dc4
KaneLeung <kk.xiaolan@gmail.com>
2020-03-05 22:39:47 +0800

    :rocket:*更新【Http】扩展类的HttpClient改成静态

--- Kane.Extension/网络类/HttpEx.cs
+++ Kane.Extension/网络类/HttpEx.cs
@@ -10,8 +10,8 @@
 * CLR 版本 ：4.0.30319.42000
 * 作　　者 ：Kane Leung
 * 创建时间 ：2020/3/3 22:21:03
-* 更新时间 ：2020/3/3 22:21:03
-* 版 本 号 ：v1.0.0.0
+* 更新时间 ：2020/3/5 22:21:03
+* 版 本 号 ：v1.0.1.0
 *******************************************************************
 * Copyright @ Kane Leung 2020. All rights reserved.
 *******************************************************************
@@ -20,6 +20,7 @@
 #if !NET40
 using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
@@ -34,6 +35,20 @@ namespace Kane.Extension
     /// </summary>
     public class HttpEx
     {
+        private static readonly HttpClient client;
+        /// <summary>
+        /// 【写成静态】带来的问题显而易见，在不重启应用的前提下，HttpClient 中的 DNS 永远不会被刷新。
+        /// </summary>
+        static HttpEx()
+        {
+#if NETCOREAPP3_0 ||　NETCOREAPP3_1
+            var handler = new HttpClientHandler() { UseCookies = true, AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate | DecompressionMethods.Brotli };
+#else
+            var handler = new HttpClientHandler() { UseCookies = true, AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate };
+#endif
+            client = new HttpClient(handler);
+        }
+
         #region 将【Json字符串】发送【POST】请求，返回【实体对象】 + PostJsonAsync<TResult>(string url, string json, string token = null)
         /// <summary>
         /// 将【Json字符串】发送【POST】请求，返回【实体对象】
@@ -81,7 +96,7 @@ namespace Kane.Extension
         /// <returns></returns>
         public static async Task<string> PostJsonAsync(string url, string json, string token = null)
         {
-            using HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Clear();
             using HttpContent content = new StringContent(json);
             if (token.IsValuable()) client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
             content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
@@ -101,7 +116,6 @@ namespace Kane.Extension
         /// <para>可参考https://docs.microsoft.com/zh-cn/dotnet/api/system.net.httprequestheader?view=netcore-3.1</para></param>
         /// <param name="headers">请求消息头</param>
         /// <param name="token">Authorization标头的Token</param>
-        /// <param name="method">压缩算法<see cref="CompressMethod"/></param>
         /// <param name="timeout">请求超时时间，默认30秒</param>
         /// <returns></returns>
         public static async Task<string> PostAsync(
@@ -110,17 +124,13 @@ namespace Kane.Extension
             string contentType = "application/json",
             Dictionary<string, string> headers = null,
             string token = null,
-            CompressMethod method = CompressMethod.None,
             int timeout = 30)
         {
-            using HttpClient client = new HttpClient
-            {
-                Timeout = new TimeSpan(0, 0, timeout)
-            };
+            client.DefaultRequestHeaders.Clear();
+            client.Timeout = new TimeSpan(0, 0, timeout);
             HttpContent content = new StringContent(data, Encoding.UTF8);
             if (headers.IsNotNull() && headers.Count > 0)
             {
-                client.DefaultRequestHeaders.Clear();
                 foreach (var item in headers)
                     client.DefaultRequestHeaders.TryAddWithoutValidation(item.Key, item.Value);
             }
@@ -128,8 +138,7 @@ namespace Kane.Extension
             if (token.IsValuable()) client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
             HttpResponseMessage response = await client.PostAsync(url, content);
             response.EnsureSuccessStatusCode();
-            return method == CompressMethod.None ? await response.Content.ReadAsStringAsync()
-                : new CompressHelper().DeCompressFromByte(await response.Content.ReadAsByteArrayAsync(), method);
+            return await response.Content.ReadAsStringAsync();
         }
         #endregion
 
@@ -155,10 +164,9 @@ namespace Kane.Extension
         /// <returns></returns>
         public static async Task<string> GetAsync(string url, string contentType = "application/json", Dictionary<string, string> headers = null, string token = null)
         {
-            using HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Clear();
             if (headers.IsNotNull() && headers.Count > 0)
             {
-                client.DefaultRequestHeaders.Clear();
                 foreach (var item in headers)
                     client.DefaultRequestHeaders.TryAddWithoutValidation(item.Key, item.Value);
             }
